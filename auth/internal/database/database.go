3661b63da264bd52faaca24dbcc393503d6fb2d9
kyoredes <kyoredes@gmail.com>
2026-01-19 00:23:21 +1000

    database fix

--- auth/internal/database/database.go
+++ auth/internal/database/database.go
@@ -2,24 +2,17 @@ package database
 
 import (
 	"auth/internal/config"
-	"context"
-	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewDatabase(DBConfig *config.DBConfig, models ...any) (*gorm.DB, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), DBConfig.DBTimeout)
-	defer cancel()
-
-	time.Sleep(1 * time.Second)
-
 	db, err := gorm.Open(postgres.Open(DBConfig.DBDSN), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 
-	return db.WithContext(ctx), nil
+	return db, nil
 
 }
